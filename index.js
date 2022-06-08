cfc3e244b0a41718c860d65b1b30da0a197d24fd
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-08 04:24:24 -0700

    commit tree 4f4b4cb4de90ece3214846097aa1fcd430fc7748

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let reply;
+  if(distance<= 400){
+    reply ='This one is on me!'
+  } else if (distance > 2200) {
+    reply = 'No can do.'
+  } else {
+    reply =  'I will gladly take your thirty bucks.'
+  }
+   return reply
 }
 
 function ternaryCheckCity(){
   // Write your code here!
-}
+  let response;
+  return response = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.'
+  }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.'
+  }
+  return response;
 }
